301e2db6572f4c741c5eed72bf9791cadcbdd3eb
Tomáš Sečkař <64587988+tommahawk25@users.noreply.github.com>
2021-06-07 21:47:44 +0200

    Update wallettest.js

--- test/wallettest.js
+++ test/wallettest.js
@@ -2,8 +2,8 @@ const Dex = artifacts.require("Dex");
 const Link = artifacts.require("Link");
 const truffleAssert = require("truffle-assertions");
 
-
 contract.skip("Dex", accounts => {
+    
     it("should only be possible for owner to add tokens", async () => {
         let dex = await Dex.deployed()
         let link = await Link.deployed()
@@ -14,6 +14,7 @@ contract.skip("Dex", accounts => {
             dex.addToken(web3.utils.sha3("AAVE"), link.address, {from: accounts[1]})
         )
     })
+    
     it("should handle deposits correctly", async () => {
         let dex = await Dex.deployed()
         let link = await Link.deployed()
@@ -22,6 +23,7 @@ contract.skip("Dex", accounts => {
         let balance = await dex.balances(accounts[0], web3.utils.sha3("LINK"))
         assert.equal(balance.toNumber(), 100) 
     })
+    
     it("should handle faulty withdrawals correctly", async () => {
         let dex = await Dex.deployed()
         let link = await Link.deployed()
@@ -29,6 +31,7 @@ contract.skip("Dex", accounts => {
             dex.withdraw(500, web3.utils.sha3("LINK"))
         )
     })
+    
     it("should handle correct withdrawals correctly", async () => {
         let dex = await Dex.deployed()
         let link = await Link.deployed()
